42a112f8ec1e64afbdc4dd8ee6865b1eb7686399
stevetoddy <stevetoddy@gmail.com>
2022-09-16 10:57:41 +1000

    First room done, answer methods done

--- crawler.py
+++ crawler.py
@@ -1,13 +1,62 @@
+import random
+
+
 class Character:
     def __init__(self, name, health = 3, attack = 1):
         self.name = name
         self.health = health
         self.attack = attack
-        # self.bag = Items(None, None, None)
+        self.turn_tracker = 0
+        self.gibberish = 0
 
+
+    def dead_end(self):
+        x = random.randint(1,101)
+        
+        if x <= 20:
+            self.turn_tracker += 1
+            return "** This door won't open **"
+        elif x > 20 and x <= 40:
+            self.turn_tracker += 1
+            return "** This door is locked, you give it a knock... No answer. **"
+        elif x > 40 and x <= 60:
+            self.turn_tracker += 1
+            return "** This door is locked tight **"
+        elif x > 60 and x <= 80:
+            self.turn_tracker += 1
+            return "** This door won't budge **"
+        elif x > 80 and x < 95:
+            self.turn_tracker += 1        
+            return "** AGH! This door is just painted on! Beautiful brushwork though. **"
+        elif x >= 95:
+            self.turn_tracker += 1        
+            return "** This door won't budge.. but wait, there's a clue tacked to the door: \"The Shop keeper is very generous if you're polite! Thank you for listening\" **"
+
+    def make_sense(self):
+        x = random.randint(1,101)
+
+        if x <= 40:
+            self.gibberish += 1
+            return "** That doesn't make sense.. **"
+        elif x > 40 and x <= 50:
+            self.gibberish += 1
+            return "** Please stop with the gibberish! **"
+        elif x > 50 and x <= 60:
+            self.gibberish += 1
+            return "** You must think before you speak **"
+        elif x > 60 and x <= 70:
+            self.gibberish += 1
+            return "** It seems like you know what you're doing, but this doesn't make sense.. **"
+        elif x > 70 and x < 95:
+            self.gibberish += 1        
+            return "** Please consider using your head.. **"
+        elif x >= 95:
+            self.gibberish += 1        
+            return "** If you don't start making sense, I will be forced to have you institutionalised! **"
+    
 
 class Bag:
     bag = []
 
-    def add(self, item):
-        self.add = self.bag.append(item)
+    def add_item(self, item):
+        self.item = self.bag.append(item)
--- main.py
+++ main.py
@@ -2,64 +2,73 @@ import crawler
 import random
 
 
+name = (input(f'Hello Contestant! What is your name? '))
+player = crawler.Character(name)
+print(f'Welcome {player.name}, let begin!\nYou enter the first room.')
 
-ans = str(input('Will you play? (y/n) ').lower().strip())
-
-if ans == 'y':
-    player = crawler.Character(input(f'Great! What is your name? '))
-    
-    print(f'Welcome {player.name}, let begin')
-    ans = str(input(f'So {player.name}, you can go left, right or forward? (l/r/f) ').lower().strip())
-    if ans == 'l':
-        print('DEAD END')
-        exit()
-    elif ans == 'r':
-        print('DEAD END')
-    else:
-        print('Right way')
-        ans = str(input('Choice between a sausage, a key or a clue? (s/k/c) ' ).lower().strip())
-        if ans == 's':
-            crawler.Bag().add('sausage')
-        elif ans == 'k':
-            crawler.Bag().add('key')
-        elif ans == 'c':
-            crawler.Bag().add('clue')
-        elif ans == 'all please':
-            crawler.Bag().add('sausage')
-            crawler.Bag().add('key')
-            crawler.Bag().add('clue')
+# FIRST ROOM 
+door_number = random.randint(1,9)
+while True:
+    print(door_number)
+    try:
+        ans = int(input('There are 8 doors with the numbers 1 to 8 painted above each. Which door do you want to go through? Pick a number between 1 and 8 to choose. '))
+        if ans == door_number:
+            print("Right way")
+            break
+        elif ans in range(1,9) and ans != door_number:
+            print(player.dead_end(),'\n')
+            continue
         else:
-            print('That doesn\'t make sense...')
+            print(player.make_sense(),'\n')
+            continue
+    except ValueError:
+        print(player.make_sense(),"\n- Enter a number between 1 and 8\n")
+        continue
+
+
+    ans = str(input('Choice between a sausage, a key or a clue? (s/k/c) ' ).lower().strip())
+    if ans == 's':
+        crawler.Bag().add_item('sausage')
+    elif ans == 'k':
+        crawler.Bag().add_item('key')
+    elif ans == 'c':
+        crawler.Bag().add_item('clue')
+    elif ans == 'all please':
+        crawler.Bag().add_item('sausage')
+        crawler.Bag().add_item('key')
+        crawler.Bag().add_item('clue')
+    else:
+        print('That doesn\'t make sense...')
 
 else:
     print('That\'s too bad, maybe next time.')
     exit()
-if len(crawler.Bag.bag) > 1:
-    ans = str(input(f'Now your bag is bulging with a {crawler.Bag.bag[0]}, a {crawler.Bag.bag[1]} and a {crawler.Bag.bag[2]}, choose between right, left or forward? (r/l/f) ').lower().strip())
-else:
-    ans = str(input(f'Now your carrying a {crawler.Bag.bag[0]} in your bag, choose between right, left or forward? (r/l/f) ').lower().strip())
-while True:
-    if ans == 'l':
-        print('Dead End')
-        exit()
-    elif ans == 'r':
-        print('dead end')
-        exit()
-    elif ans == 'f':
-        print('Right way')
-        break
-    else:
-        print('That doesn\'t make sense..')
-        continue
+# if len(crawler.Bag.bag) > 1:
+#     ans = str(input(f'Now your bag is bulging with a {crawler.Bag.bag[0]}, a {crawler.Bag.bag[1]} and a {crawler.Bag.bag[2]}, choose between right, left or forward? (r/l/f) ').lower().strip())
+# else:
+#     ans = str(input(f'Now your carrying a {crawler.Bag.bag[0]} in your bag, choose between right, left or forward? (r/l/f) ').lower().strip())
+# while True:
+#     if ans == 'l':
+#         print('Dead End')
+#         exit()
+#     elif ans == 'r':
+#         print('dead end')
+#         exit()
+#     elif ans == 'f':
+#         print('Right way')
+#         break
+#     else:
+#         print('That doesn\'t make sense..')
+#         continue
 
-x = random.randint(1,100)
-if x <= 30:
-    print('You see something moving in the darkness...\n')
-    ans = str(input(f'As it moves into the light you can see it\'s a hungry dog... Do you run, play dead, or give him your {crawler.Bag.bag[0]}? (run, play, sausage)').lower().strip())
-elif x > 30 and x < 70:
-    print('Face door')
-elif x == 100:
-    print('SECRET')
-else:
-    print('Face Riddle')
+# x = random.randint(1,100)
+# if x <= 30:
+#     print('You see something moving in the darkness...\n')
+#     ans = str(input(f'As it moves into the light you can see it\'s a hungry dog... Do you run, play dead, or give him your {crawler.Bag.bag[0]}? (run, play, sausage)').lower().strip())
+# elif x > 30 and x < 70:
+#     print('Face door')
+# elif x == 100:
+#     print('SECRET')
+# else:
+#     print('Face Riddle')
     
